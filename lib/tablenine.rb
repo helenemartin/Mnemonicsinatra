66ee9eb46bf45b8c49bc5051e35b18350bf7d243
HeleneMartin <helene.martin@fundingcircle.com>
2014-04-17 12:43:19 +0100

    add css stylesheet and create, style index.erb page

--- lib/tablenine.rb
+++ lib/tablenine.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
  
 class Table < Sinatra::Base
+  use Rack::Static, :urls => ['/images', '/css'], :root => 'public'
 
   get "/" do
     erb :index
--- lib/views/tablenine.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra/base'
- 
-class Table < Sinatra::Base
-  use: Rack::Static, :urls => ['/images', '/css'], :root => 'public'
-
-  get "/" do
-    erb :index
-  end
-end
