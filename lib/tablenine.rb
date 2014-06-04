a832059a56b25ca256e7b42bf560b7ddf711087f
helenemartin <helenemartin13@hotmail.com>
2014-06-04 21:09:43 +0100

    create calculation rb, link image to calculation input

--- lib/tablenine.rb
+++ lib/tablenine.rb
@@ -3,7 +3,15 @@ require 'sinatra/base'
 class Table < Sinatra::Base
   use Rack::Static, :urls => ['/images', '/css'], :root => 'public'
 
-  get "/" do
+  get '/' do
     erb :index
   end
+
+  post '/submit' do
+    # Get the number the user is trying to multiply by 9
+    @number = params[:message].to_i
+    @calculation = @number * 9
+    erb :calculation
+  end
+
 end
